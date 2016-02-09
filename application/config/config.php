387d4820e5c6a72adabbc292a11eef6caeedff75
johnstone410 <albert.caminero@gmail.com>
2016-02-09 20:57:45 +0800

    modified

--- application/config/config.php
+++ application/config/config.php
@@ -18,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] 	= 'http://localhost:8888/bahaypalitan/';
+$config['base_url'] 	= 'http://localhost/bahaypalitan/';
 
 /*
 |--------------------------------------------------------------------------
